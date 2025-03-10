3473927a3e4172c6e3a44f85e4089dfe6d750812
Andrei Beriukhov <beriukhov@gmail.com>
2025-03-10 19:23:31 +0200

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -126,7 +126,6 @@ dependencies {
 
     implementation(libs.vico.compose)
     implementation(libs.vico.compose.m3)
-    implementation(libs.vico.core)
 
     // Wearable
     implementation(libs.playServices.wearable)
