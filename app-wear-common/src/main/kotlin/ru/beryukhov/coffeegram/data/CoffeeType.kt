f47a9a70677c3fe4634ee321ba5e7e114735405c
Andrei Beriukhov <beriukhov@gmail.com>
2025-03-19 16:31:03 +0200

    Extract CoffeeType interface

--- app-wear-common/src/main/kotlin/ru/beryukhov/coffeegram/data/CoffeeType.kt
+++ app-wear-common/src/main/kotlin/ru/beryukhov/coffeegram/data/CoffeeType.kt
@@ -4,10 +4,17 @@ import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import ru.beryukhov.coffeegram.common.R
 
-enum class CoffeeType(
-    @StringRes val nameId: Int,
-    @DrawableRes val iconId: Int = R.drawable.coffee
-) {
+interface CoffeeType {
+    @get:StringRes val nameId: Int
+    @get:DrawableRes val iconId: Int
+    val key: String
+}
+
+enum class CoffeeTypes(
+    @StringRes override val nameId: Int,
+    @DrawableRes override val iconId: Int = R.drawable.coffee,
+
+): CoffeeType {
     Cappuccino(R.string.cappuccino, R.drawable.cappuccino),
     Latte(R.string.latte, R.drawable.latte),
     Americano(R.string.americano, R.drawable.americano),
@@ -19,9 +26,11 @@ enum class CoffeeType(
     Fredo(R.string.fredo, R.drawable.fredo),
     Irish(R.string.irish, R.drawable.irish),
     Cocoa(R.string.cocoa, R.drawable.cocoa),
-    Chocolate(R.string.chocolate, R.drawable.chocolate),
+    Chocolate(R.string.chocolate, R.drawable.chocolate),;
     // icons from here: https://www.freepik.com/free-vector/list-different-types-coffee_951047.htm
     // app logo is here: https://www.flaticon.com/free-icon/coffee-cup_766408
+
+    override val key: String = this.name
 }
 
 data class CoffeeTypeWithCount(
--- app/src/main/java/ru/beryukhov/coffeegram/view/CoffeeTypeItem.kt
+++ app/src/main/java/ru/beryukhov/coffeegram/view/CoffeeTypeItem.kt
@@ -31,7 +31,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ru.beryukhov.coffeegram.data.CoffeeType
-import ru.beryukhov.coffeegram.data.CoffeeType.Cappuccino
+import ru.beryukhov.coffeegram.data.CoffeeTypes.Cappuccino
 
 @Composable
 fun CoffeeTypeItem(
